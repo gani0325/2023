6c961dfe811614b19a69ae93968869b41fbc07af
gani0325 <lge0325@bme.gachon.ac.kr>
2023-01-01 23:52:57 +0900

    ➕[Feat] point 공부

---
+++ Image processing/C++/point3.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+
+int main() {
+	// 객체 기본 및 간결 선언 방식
+	Point3_<int> pt1(100, 200, 300);
+	Point3_<float> pt2(92.3f, 125.23f, 250.f);
+	Point3f pt3(0.3f, 0.f, 15.7f);
+	Point3d pt4(0.25, 0.6, 33.3);
+
+	// 객체 간결 선언 및 객체 연산 -> i, f, d
+	Point3i pt5 = pt1 - (Point3i)pt2;
+	Point3f pt6 = pt2 * 3.14f;
+	Point3d pt7 = (pt4 + (Point3d)pt3) * 10.f;
+
+	// std::cout : 다양한 클래스의 원소를 쉽게 출력
+	cout << "두 벡터(pt4, pt7)의 내적 " << pt4.dot(pt7) << endl;
+	cout << "pt5 = " << pt5.x << ", " << pt5.y << ", " << pt5.z << endl;
+	cout << "[pt6] = " << pt6 << endl;
+	cout << "[pt7] = " << pt7 << endl;
+
+	return 0;
+}
