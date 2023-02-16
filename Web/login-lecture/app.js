bbd9ef9e660ec33eb980c0542545557f1fdffe2f
gani <lge000325@gmail.com>
2023-02-16 16:40:46 +0900

    [🚧 Refactor] js 루트 추가

--- Web/login-lecture/app.js
+++ Web/login-lecture/app.js
@@ -5,14 +5,17 @@ const express = require("express");
 const app = express();
 
 // 라우터
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // 앱 세팅
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+// ejs 에서 js 파일 받아올 수 있도록 미들웨어
+app.use(express.static(`${__dirname}/src/public`))
 // routes 꺼 받아오기
 app.use("/", home);      // use -> 미들 웨어를 등록해주는 메서드
+
 
 // app.js 내보내기
 module.exports = app;
