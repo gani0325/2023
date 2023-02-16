a7d819046e6c8df1849cbc93552ec2785e08144b
gani <lge000325@gmail.com>
2023-02-16 16:57:31 +0900

    [➕ Feat] login하면 value 값 받아오기

--- Web/login-lecture/src/public/js/home/login.js
+++ Web/login-lecture/src/public/js/home/login.js
@@ -1,3 +1,20 @@
 "use strict";
+// DOM
+// Document Object Model
+// 자바스크립트에서 html의 존재하는 데이터들을 가져와서 제어하기
+const user_id = document.querySelector("#user_id");      // 선택자 -> HTML의 태그들!!
+const user_pw = document.querySelector("#user_pw");
+const loginBtn = document.querySelector("button");
 
+console.log(user_id);
 console.log("hello");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id : user_id.value,
+        pw : user_pw.value
+    };
+    console.log(req);
+}
