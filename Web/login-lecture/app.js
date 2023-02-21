fdc29079989fe6f0211bbf4318e4fed8b383c2db
gani <lge0325@bme.gachon.ac.kr>
2023-02-21 16:37:01 +0900

    ➕[Feat] morgan으로 log 저장하기

--- Web/login-lecture/app.js
+++ Web/login-lecture/app.js
@@ -4,12 +4,15 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
+const morgan = require("morgan");
+const fs = require("fs");
 
 const app = express();
+dotenv.config();
 
 // 라우터
 const home = require("./src/routes/home");
+const accessLogStream = require("./src/config/log");
 
 // 앱 세팅
 app.set("views", "./src/views");
@@ -20,6 +23,8 @@ app.use(express.static(`${__dirname}/src/public`))
 app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded( {extended : true}));
+// morgan middleware, stream 연결 통로 (tiny, :method, dev, combined, : method :date[web] 등 사용가능)
+app.use(morgan("common", { stream : accessLogStream}));
 // routes 꺼 받아오기
 app.use("/", home);      // use -> 미들 웨어를 등록해주는 메서드
 
