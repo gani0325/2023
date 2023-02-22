91ba4f69759fb86a7c5aea76835508dfc811ecc3
gani <lge0325@bme.gachon.ac.kr>
2023-02-22 16:30:28 +0900

    ➕[Feat] 화면에 props로 기본정보 띄우기

---
+++ Web/management/src/components/Customer.js
@@ -0,0 +1,14 @@
+import React from "react";
+class Customer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.name}</h1>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+            </div>
+        );
+    }
+}
+
+export default Customer;
