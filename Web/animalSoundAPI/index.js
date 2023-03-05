a6b8db20ed9ea3917ce70d096efec7674d895034
gani <lge0325@bme.gachon.ac.kr>
2023-03-05 23:33:29 +0900

    ➕[Feat] 간단한 html에 백엔드 연동하기

--- Web/animalSoundAPI/index.js
+++ Web/animalSoundAPI/index.js
@@ -1,25 +1,23 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const port = 3000
+
+app.use(cors())
 
 // HTTP메소드("라우팅", () => {} 콜백함수)
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/user/:id', function (req, res) {
-  // 1) get의 params -> q가 id임, :뒤
-  const p = req.params
-  console.log(p)
-  // 2) get의 query -> ?사용한후 뒤에 key value 사용! -> /id?q=~~&name=~~ { q: 'hi', name: 'gani' }
-  const q = req.query
-  console.log(q)
-
-  res.json({"sound" : "멍멍"})
-})
-
-app.get('/cat', function (req, res) {
-  res.send({"sound" : "야옹"})
+app.get('/sound/:name', function (req, res) {
+  const { name } = req.params;     // params의 key를 {} 넣으면 바로 적용됨
+  console.log(name);
+  if (name == "dog") {
+    res.json({"sound" : "멍멍"})
+  } else {
+    res.json({"sound" : "알수 없는 울음소리"})
+  }
 })
 
 app.listen(port, () => {
