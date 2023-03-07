94ebfda3e1c431e3795c48a4f129fa5f67283c75
gani <lge0325@bme.gachon.ac.kr>
2023-03-08 00:43:57 +0900

    🚀[Chore] MongoDB 연결

--- Web/ImageUpload/index.js
+++ Web/ImageUpload/index.js
@@ -7,10 +7,16 @@ const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Database 연결
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });   // useNewUrlParser : 에러 방지
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("💚 Connected to the database!"));
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.listen(PORT, () => {
-    console.log(`server started at http://localhost:${PORT}`);
+  console.log(`server started at 🚀 http://localhost:${PORT}`);
 });
