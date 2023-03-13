17b95d297ba47e632ec574a3a94b51bbbb2cb5f8
gani <lge0325@bme.gachon.ac.kr>
2023-03-13 23:07:06 +0900

    ➕[Feat] router 통해 로그인, 회원가입 페이지 GET

--- Web/PassportAuthentication/app.js
+++ Web/PassportAuthentication/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 // Routes
 app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/user"));
 
 const PORT = process.env.PORT || 8000;
 
--- Web/PassportAuthentication/routes/index.js
+++ Web/PassportAuthentication/routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// Main page
 router.get("/", (req, res) => {
     res.send("Welcome home");
-})
+});
 
 module.exports = router;
