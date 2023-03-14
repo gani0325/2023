d71e4257d8f10af1481502b166f42c66b4809b6e
gani <lge0325@bme.gachon.ac.kr>
2023-03-14 23:44:14 +0900

    ➕[Feat] ejs로 welcome 페이지 띄우기

--- Web/PassportAuthentication/app.js
+++ Web/PassportAuthentication/app.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
 const app = express();
 
+// ejs 미들웨어
+app.use(expressLayouts);
+// express 의 view 엔진을 ejs 로 세팅
+app.set("view engine", "ejs");
+//  set 을 한 부분은 layout 폴더의 layout 파일을 사용하겠다는 선언
 // Routes
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/user"));
--- Web/PassportAuthentication/routes/index.js
+++ Web/PassportAuthentication/routes/index.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 // Main page
 router.get("/", (req, res) => {
-    res.send("Welcome home");
+    // rednering 할 때 view의 파일 이름 써야됨
+    res.render("welcome");
 });
 
 module.exports = router;
