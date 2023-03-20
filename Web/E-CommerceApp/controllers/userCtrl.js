af2da78dc0aad41cd0878f066a6a57b70ee0b9bf
gani <lge0325@bme.gachon.ac.kr>
2023-03-21 01:35:27 +0900

    ➕[Feat] bcrypt로 비밀번호 해쉬

--- Web/E-CommerceApp/controllers/userCtrl.js
+++ Web/E-CommerceApp/controllers/userCtrl.js
@@ -1,21 +1,27 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const asyncHandler = require("express-async-handler");
 
-const createUser = async (req, res) => {
-  const email = req.body.email;
-  const findUser = await User.findOne({email : email});
+const createUser = asyncHandler(async (req, res) => {
+  const { firstname, lastname, email, mobile, password } = req.body;
+  const findUser = await User.findOne({ email: email });
 
   // email이 db에 없다면
   if (!findUser) {
-    // Create
-    const newUser = await User.create(req.body);
+    // Create a new User
+
+    // 1) 우선 비밀번호 해쉬화(암호화)
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // 2) 새 User 정보 만들기
+    const newUser = await User.create({
+      firstname, lastname, email, mobile, password: hashedPassword
+    });
+
     res.json(newUser);
   } else {
-    // User already existed
-    res.json({
-      msg: "User already existed!",
-      success: false,
-    });
+    // User already exists
+    throw new Error("User already exists");
   }
-};
+});
 
-module.exports = createUser;
+module.exports = { createUser };
