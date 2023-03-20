415a22b3c131e673edd72c7c38f91cc60920074d
gani <lge0325@bme.gachon.ac.kr>
2023-03-21 00:45:36 +0900

    ➕[Feat] User의 Schema

---
+++ Web/E-CommerceApp/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+}, { collection: 'Commerce' });
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
