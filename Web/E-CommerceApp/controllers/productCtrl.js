24d8fffae3360f6d90bbc18520bc1994399e73fe
gani <lge0325@bme.gachon.ac.kr>
2023-03-23 00:56:21 +0900

    ➕[Feat] Product의 post 및 get

---
+++ Web/E-CommerceApp/controllers/productCtrl.js
@@ -0,0 +1,26 @@
+const Product = require("../models/Product");
+const asyncHandler = require("express-async-handler");
+
+// 상품 등록
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const findProduct = await Product.findById(id);
+    res.json(findProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+})
+module.exports = {
+  createProduct,
+  getProduct
+};
