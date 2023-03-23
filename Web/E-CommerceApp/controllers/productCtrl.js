1a299bdb919596473b3c52c842c9bf049a41f60e
gani <lge0325@bme.gachon.ac.kr>
2023-03-24 00:43:30 +0900

    ➕[Feat] Product 수정 (PUT)

--- Web/E-CommerceApp/controllers/productCtrl.js
+++ Web/E-CommerceApp/controllers/productCtrl.js
@@ -1,11 +1,32 @@
 const Product = require("../models/Product");
 const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
 
 // 상품 등록
 const createProduct = asyncHandler(async (req, res) => {
   try {
     const newProduct = await Product.create(req.body);
     res.json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// 상품 수정
+const updateProduct = asyncHandler(async (req, res) => {
+  const {id} =req.params;
+  try {
+    if (req.body.title) {
+      // slugify : 텍스트를 url 주소로 변환해주는 라이브러리
+      // slug : 이미 얻은 데이터를 사용하여 유효한 URL을 생성 (URL과 의미있는 이름을 사용)
+      req.body.slug = slugify(req.body.title);
+    }
+    const updateProduct = await Product.findOneAndUpdate(id,
+      req.body, {
+      new: true,
+    });
+    console.log(updateProduct);
+    res.json(updateProduct);
   } catch (error) {
     throw new Error(error);
   }
@@ -34,6 +55,7 @@ const getAllProduct = asyncHandler(async (req, res) => {
 
 module.exports = {
   createProduct,
+  updateProduct,
   getAProduct,
   getAllProduct
 };
--- Web/E-CommerceApp/routes/productRoute.js
+++ Web/E-CommerceApp/routes/productRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createProduct, getAProduct, getAllProduct } = require("../controllers/productCtrl");
+const { createProduct, getAProduct, getAllProduct, updateProduct } = require("../controllers/productCtrl");
 const router = express.Router();
 
 router.post("/", createProduct);
+
 router.get("/:id", getAProduct);
+router.put("/:id", updateProduct);
 router.get("/", getAllProduct);
+
+
 module.exports = router;
