189cd2b8ed3c2f6cc9f534e8a2dfc584f83382e0
gani0325 <lge0325@bme.gachon.ac.kr>
2023-03-25 01:07:48 +0900

    🚧[Refactir] 비밀번호 재생성에 필요한 Schema

--- Web/E-CommerceApp/models/User.js
+++ Web/E-CommerceApp/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 
 const UserSchema = new mongoose.Schema({
   firstname: {
@@ -46,10 +48,51 @@ const UserSchema = new mongoose.Schema({
   refreshToken: {
     type: String,
   },
+  passwordChangedAt : {
+    type : Date
+  },
+  passwordResetToken : {
+    type : String
+  },
+  passwordResetExpires : {
+    type : Date
+  }
 }, {
   timestamps: true,
   collection: 'users'
 });
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  // Create a new User
+  // 1) 우선 비밀번호 해쉬화(암호화)
+  const salt = await bcrypt.genSaltSync(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// check password matching
+UserSchema.methods.isPasswordMatched = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// return JSON Web Token
+UserSchema.methods.createPasswordResetToken = function () {
+  // Generate Token
+  const resetToken = crypto.randomBytes(32).toString("hex");
+  
+  // Hash token
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  
+    // set expired time
+  this.passwordResetExpires = Date.now() + 30 * 60 * 1000   // 10분
+  return resetToken;
+};
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
