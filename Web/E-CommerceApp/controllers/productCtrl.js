f91b27dc04011ba930797e43e5926d49364f8eba
gani0325 <lge0325@bme.gachon.ac.kr>
2023-03-24 23:29:35 +0900

    🚧[Refactor] 4가지 방법으로 상품 조회

--- Web/E-CommerceApp/controllers/productCtrl.js
+++ Web/E-CommerceApp/controllers/productCtrl.js
@@ -19,7 +19,7 @@ const createProduct = asyncHandler(async (req, res) => {
 
 // 상품 수정
 const updateProduct = asyncHandler(async (req, res) => {
-  const {id} =req.params;
+  const { id } = req.params;
   try {
     if (req.body.title) {
       // slugify : 텍스트를 url 주소로 변환해주는 라이브러리
@@ -38,7 +38,7 @@ const updateProduct = asyncHandler(async (req, res) => {
 
 // 상품 삭제
 const deleteProduct = asyncHandler(async (req, res) => {
-  const {id} =req.params;
+  const { id } = req.params;
   try {
     if (req.body.title) {
       // slugify : 텍스트를 url 주소로 변환해주는 라이브러리
@@ -66,18 +66,49 @@ const getAProduct = asyncHandler(async (req, res) => {
 // 모든 상품 조회
 const getAllProduct = asyncHandler(async (req, res) => {
   try {
-    // 1) Filtering
-    const queryObj = {...req.query};
+    // 1) Filtering => t?price[lt]=50000
+    const queryObj = { ...req.query };
     const excludeFields = ["page", "sort", "limit", "fields"];
     excludeFields.forEach((el) => delete queryObj[el])
     console.log(queryObj);
-    
+
     // JSON.stringify : JavaScript 값이나 객체를 JSON 문자열로 변환
     let queryStr = JSON.stringify(queryObj);
     // Create operators ($gt, $gte, etc)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
     // JSON.parse : JSON 문자열을 인자로 받고 결과값으로 JavaScript 객체를 반환
-    const query = Product.find(JSON.parse(queryStr));
+    let query = Product.find(JSON.parse(queryStr));
+
+    // 2)Sorting => ?sort=-category,-brand
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort("-createdAt");
+    }
+
+    // 3) Limiting the fields => ?fields=-title,-price,-category
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
+    // 4) pagination  => ?page=4&limit=5
+    // 페이지 나누기, 쿼리의 결과값으로 리턴된 리소스를 분할하여 전달
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) {
+        throw new Error("This page does not exists");
+      }
+    }
+    // console.log(page, limit, skip);
+
     const product = await query;
     res.json(product);
   } catch (error) {
