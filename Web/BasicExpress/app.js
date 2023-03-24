8f33c5b9c2335f09ae39c4c1568c3badb3737960
gani <lge0325@bme.gachon.ac.kr>
2023-03-24 12:13:11 +0900

    ➕[Feat] middleware 구조 이해하기

--- Web/BasicExpress/app.js
+++ Web/BasicExpress/app.js
@@ -4,16 +4,27 @@ const PORT = process.env.PORT || 8000;
 const path = require("path");
 const app = express();
 
+app.use((req, res, next) => {
+  console.log("모든 요청에 다 실행됨");
+  // next : 다음 미들웨어로 간다
+  next();
+});
+
 // get(주소, 라우터) : 주소에 대한 요청이 올 때 어떤 동작을 하나
-app.get("/", (req, res) => {
-    // 1) app.js 로 서버 열기
-    // res.send("Express study");
-    
-    // 2) index.html 로 서버 열기
-    res.sendFile(path.join(__dirname, "/index.html"));
+app.get("/", (req, res, next) => {
+
+  console.log("GET / 요청에서만 실행");
+  next();
+}, (req, res) => {
+  throw new Error("Error는 Error 처리 Middleware로 간다");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
 });
 
 // listen = http 웹 서버
 app.listen(PORT, () => {
-    console.log(`🚀 Server started on port http://localhost:${PORT}`);
+  console.log(`🚀 Server started on port http://localhost:${PORT}`);
 });
