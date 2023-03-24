3ee22ff8725af28164ed50f6e2180cb757b9de1f
gani <lge0325@bme.gachon.ac.kr>
2023-03-24 12:56:10 +0900

    ➕[Feat] multer로 파일 업로드 해보기

--- Web/BasicExpress/app.js
+++ Web/BasicExpress/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const morgan = require("morgan");
 const cookiePraser = require("cookie-parser");
 const session = require("express-session");
+const multer = require("multer");
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -11,18 +12,44 @@ const app = express();
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookiePraser(process.env.SECRET));
 app.use(session({
-  resave : false,
-  saveUninitialized : false,
-  secret : process.env.SECRET,
-  cookie : {
-    httpOnly : true,
-    secure : false,
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
   },
-  name : "session-cookie",
+  name: "session-cookie",
 }));
+
+const upload = multer({
+  storage: multer.diskStorage({
+    // 어디에 어떤 이름으로 저장되는지
+    destination(req, file, done) {
+      done(null, "uploads/");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+app.get("/upload", (req, res) => {
+  res.sendFile(path.join(__dirname, "multipart.html"));
+});
+
+app.post("/upload",
+  upload.fields([{ name: "image1" }, { name: "image2" }]),
+  (req, res) => {
+    console.log(req.files, req.body);
+    res.send("ok");
+  },
+);
 
 app.use((req, res, next) => {
   console.log("모든 요청에 다 실행됨");
