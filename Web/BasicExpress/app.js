a2a539125758e93edb49258cde0efa68cf2e864f
gani <lge0325@bme.gachon.ac.kr>
2023-03-24 13:19:19 +0900

    ➕[Feat] router 분리해서 main, user 띄우기

--- Web/BasicExpress/app.js
+++ Web/BasicExpress/app.js
@@ -4,7 +4,8 @@ const path = require("path");
 const morgan = require("morgan");
 const cookiePraser = require("cookie-parser");
 const session = require("express-session");
-const multer = require("multer");
+const indexRouter = require("./routes/index");
+const userRouter = require("./routes/user");
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -25,37 +26,15 @@ app.use(session({
   name: "session-cookie",
 }));
 
-const upload = multer({
-  storage: multer.diskStorage({
-    // 어디에 어떤 이름으로 저장되는지
-    destination(req, file, done) {
-      done(null, "uploads/");
-    },
-    filename(req, file, done) {
-      const ext = path.extname(file.originalname);
-      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 },
-});
-
-app.get("/upload", (req, res) => {
-  res.sendFile(path.join(__dirname, "multipart.html"));
-});
-
-app.post("/upload",
-  upload.fields([{ name: "image1" }, { name: "image2" }]),
-  (req, res) => {
-    console.log(req.files, req.body);
-    res.send("ok");
-  },
-);
 
 app.use((req, res, next) => {
   console.log("모든 요청에 다 실행됨");
   // next : 다음 미들웨어로 간다
   next();
 });
+
+app.use("/", indexRouter);
+app.use("/user", userRouter);
 
 // listen = http 웹 서버
 app.listen(PORT, () => {
