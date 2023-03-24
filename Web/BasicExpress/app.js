b53a28b02f5643b59d862dafe1b3d43e118f33d0
gani <lge0325@bme.gachon.ac.kr>
2023-03-24 12:40:30 +0900

    ➕[Feat] 미들웨어 패키지 활용해보기

--- Web/BasicExpress/app.js
+++ Web/BasicExpress/app.js
@@ -1,27 +1,33 @@
 const express = require("express");
 require("dotenv").config();
-const PORT = process.env.PORT || 8000;
 const path = require("path");
+const morgan = require("morgan");
+const cookiePraser = require("cookie-parser");
+const session = require("express-session");
+
+const PORT = process.env.PORT || 8000;
 const app = express();
+
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookiePraser(process.env.SECRET));
+app.use(session({
+  resave : false,
+  saveUninitialized : false,
+  secret : process.env.SECRET,
+  cookie : {
+    httpOnly : true,
+    secure : false,
+  },
+  name : "session-cookie",
+}));
 
 app.use((req, res, next) => {
   console.log("모든 요청에 다 실행됨");
   // next : 다음 미들웨어로 간다
   next();
-});
-
-// get(주소, 라우터) : 주소에 대한 요청이 올 때 어떤 동작을 하나
-app.get("/", (req, res, next) => {
-
-  console.log("GET / 요청에서만 실행");
-  next();
-}, (req, res) => {
-  throw new Error("Error는 Error 처리 Middleware로 간다");
-});
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send(err.message);
 });
 
 // listen = http 웹 서버
