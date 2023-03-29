ca4f961f213d88b241fea9eae3384ba49d322fba
gani0325 <lge0325@bme.gachon.ac.kr>
2023-03-29 23:40:08 +0900

    ➕[Feat] 블로그 업데이트 (PUT)

--- Web/E-CommerceApp/controllers/blogCtrl.js
+++ Web/E-CommerceApp/controllers/blogCtrl.js
@@ -3,13 +3,24 @@ const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 const { validateMongodbID } = require("../utils/validateMongodbID");
 
+// 블로그 생성하기
 const createBlog = asyncHandler(async (req, res) => {
   try {
     const newBlog = await Blog.create(req.body);
-    res.json({
-      status: "success",
-      newBlog,
-    });
+    res.json(newBlog);
+  }
+  catch (error) {
+    throw new Error(error);
+  }
+});
+
+// 블로그 수정하기
+const updateBlog = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongodbID(id);
+  try {
+    const updateBlog = await Blog.findByIdAndUpdate(id, req.body);
+    res.json(updateBlog);
   }
   catch (error) {
     throw new Error(error);
@@ -17,5 +28,7 @@ const createBlog = asyncHandler(async (req, res) => {
 });
 
 module.exports = {
-  createBlog
+  createBlog,
+  updateBlog,
+
 }
--- Web/E-CommerceApp/routes/blogRoute.js
+++ Web/E-CommerceApp/routes/blogRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createBlog } = require("../controllers/blogCtrl");
+const { createBlog, updateBlog } = require("../controllers/blogCtrl");
 const { authMiddleware, isAdmin } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, isAdmin, createBlog);
+router.put("/:id", updateBlog);
 
 module.exports = router;
