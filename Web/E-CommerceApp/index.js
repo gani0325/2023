4aa40186c60f0dcf32c78f5fc738a024d8b842e2
gani0325 <lge0325@bme.gachon.ac.kr>
2023-03-31 01:02:25 +0900

    ➕[Feat] 카테고리 라우터 추가

--- Web/E-CommerceApp/index.js
+++ Web/E-CommerceApp/index.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 8000;
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
+const categoryRouter = require("./routes/categoryRoute");
 const cookieParser = require("cookie-parser");
 
 // mongoDB
@@ -23,6 +24,7 @@ app.use(cookieParser());
 app.use("/api/user", authRouter);
 app.use("/api/product", productRouter);
 app.use("/api/blog", blogRouter);
+app.use("/api/category", categoryRouter);
 
 app.use(notFound);
 app.use(errorHandler);
