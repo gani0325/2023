4dd802e49ce55e57d7f8c8b6fe5bf31d8f1dc607
gani0325 <lge0325@bme.gachon.ac.kr>
2023-04-22 20:45:20 +0900

    🚧[Refactor] 워커 프로세스로 요청하기

--- Web/httpSever/cluster.js
+++ Web/httpSever/cluster.js
@@ -27,6 +27,11 @@ if (cluster.isMaster) {
     res.write("<h1>Hello node!</h1>");
     // 응답을 종하는 메서드
     res.end("<p>Hello Server!</p>");
+    setTimeout(() => {
+      // 워커가 존재하는지 확인하기 위해 1초마다 강제 종료
+      process.exit(1);
+    }, 1000);
+
   }).listen(8086);
 
   console.log(`${process.pid}번 워커 실행`);
