da92a1bc2f45ba6967849d8d4fb7ceb290de875b
gani0325 <lge0325@bme.gachon.ac.kr>
2023-04-22 20:46:20 +0900

    🚧[Refactor] 워커 종료시키기 추가

--- Web/httpSever/cluster.js
+++ Web/httpSever/cluster.js
@@ -13,6 +13,8 @@ if (cluster.isMaster) {
   cluster.on("exit", (worker, code, signal) => {
     console.log(`${worker.process.pid}번 워커가 종료되었습니다.`);
     console.log("code", code, "signal", signal);
+    // 워커 종료시키기
+    cluster.fork();
   });
 } else {
   // 워커들이 포트에서 대기
