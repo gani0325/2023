b65723ca1fab68c10d306b0ac9eb3650eacd63e2
gani0325 <lge0325@bme.gachon.ac.kr>
2023-04-23 23:24:32 +0900

    ✉️[Comment] http 서버 간단한 설명 추가

--- Web/httpSever/app.js
+++ Web/httpSever/app.js
@@ -1,12 +1,18 @@
+// express : http 모듈 내장되어서 서버의 역할
 const express = require("express");
 const app = express();
 require("dotenv").config();
 
+// 서버가 실행될 포트
+// app.set("키, 값") : 데이터 저장
 app.set("port", process.env.PORT || 3000);
 
+// app.get("주소, 라우터") : 주소에 대한 GET 요청이 올 때 어떤 동작 할지 적기
 app.get("/", (req, res) => {
     res.send("Hello express");
 });
+
+// HTTP 웹 서버와 동일
 app.listen(app.get("port"), () => {
     console.log(app.get("port"), "빈 포트에서 대기중");
 });
