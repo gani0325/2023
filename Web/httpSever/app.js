ae80643c6b19b986aa54d3997daf4e6b1af15fac
gani0325 <lge0325@bme.gachon.ac.kr>
2023-04-23 23:28:43 +0900

    ➕[Feat] html로 서버 응답하기

--- Web/httpSever/app.js
+++ Web/httpSever/app.js
@@ -1,6 +1,8 @@
 // express : http 모듈 내장되어서 서버의 역할
 const express = require("express");
 const app = express();
+// 파일 경로
+const path = require("path");
 require("dotenv").config();
 
 // 서버가 실행될 포트
@@ -9,7 +11,8 @@ app.set("port", process.env.PORT || 3000);
 
 // app.get("주소, 라우터") : 주소에 대한 GET 요청이 올 때 어떤 동작 할지 적기
 app.get("/", (req, res) => {
-    res.send("Hello express");
+    // html로 응답하기
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 // HTTP 웹 서버와 동일
