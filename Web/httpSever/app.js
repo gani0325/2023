019750547358ae5dad367038801727296fbeff91
gani0325 <lge0325@bme.gachon.ac.kr>
2023-04-25 23:41:41 +0900

    ➕[Feat] morgan 으로 dev 실행하기

--- Web/httpSever/app.js
+++ Web/httpSever/app.js
@@ -1,13 +1,37 @@
 // express : http 모듈 내장되어서 서버의 역할
 const express = require("express");
 const app = express();
+// process.env 관리
+const dotenv = require("dotenv");
+// 미들웨어
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const morgan = require("morgan");
 // 파일 경로
 const path = require("path");
 require("dotenv").config();
 
+dotenv.config();
 // 서버가 실행될 포트
 // app.set("키, 값") : 데이터 저장
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 8000);
+
+// dev : [HTTP 메서드][주소][HTTP 상태 코드][응답 코드]-[응답 바이트]
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: "session-cookie",
+}));
 
 // 미들웨어 : 요청과 응답을 조정
 // 요청, 응답, 다음 미들에ㅜ어로 넘어가기
@@ -30,5 +54,5 @@ app.use((err, req, res, next) => {
 
 // HTTP 웹 서버와 동일
 app.listen(app.get("port"), () => {
-    console.log(app.get("port"), "빈 포트에서 대기중");
+  console.log(app.get("port"), "빈 포트에서 대기중");
 });
