e736efe77e9141bcb435149617699196eda67176
gani0325 <lge0325@bme.gachon.ac.kr>
2023-04-28 18:02:26 +0900

    ➕[Feat] DB 와 router Setup

--- Web/Fullstack-E-commerceApp/server.js
+++ Web/Fullstack-E-commerceApp/server.js
@@ -1,12 +1,27 @@
-const express = require("express");
-const colors = require("colors");
-const dotenv = require("dotenv");
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+// router
+import authRouter from "./routes/authRouter.js";
 
 // configure env
 dotenv.config();
 
+// Databse config
+connectDB();
+
 // rest object
 const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+// routes
+app.use("/api/v1/auth", authRouter);
 
 // rest api
 app.get("/", (req, res) => {
@@ -19,6 +34,7 @@ const PORT = process.env.PORT || 8000;
 // run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} http://localhost:${PORT}`
+    `Server Running on ${process.env.DEV_MODE} http://localhost:${PORT}`.bgCyan
+      .white
   );
 });
