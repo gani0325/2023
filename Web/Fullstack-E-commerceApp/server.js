e19945e61f425e8bfb1d6f39ccb219392ebaf77a
gani0325 <lge0325@bme.gachon.ac.kr>
2023-04-28 13:10:57 +0900

    🚧[Refactor] configure env 수정하기

--- Web/Fullstack-E-commerceApp/server.js
+++ Web/Fullstack-E-commerceApp/server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// configure env
+dotenv.config();
 
 // rest object
 const app = express();
 
 // rest api
 app.get("/", (req, res) => {
-  res.send({
-    message: "Welcome to ecommerce app",
-  });
+  res.send("<h>Welcome ecommerce app</h>");
 });
 
 // PORT
@@ -15,5 +18,7 @@ const PORT = process.env.PORT || 8000;
 
 // run listen
 app.listen(PORT, () => {
-  console.log(`Server Running on http://localhost:${PORT}`);
+  console.log(
+    `Server Running on ${process.env.DEV_MODE} http://localhost:${PORT}`
+  );
 });
