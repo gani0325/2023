2ef13f999e4dd93cde02f593e54c9e2660a17fa0
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-03 01:15:27 +0900

    ➕[Feat] Layout의 Footer.js

--- Web/Fullstack-E-commerceApp/client/src/components/Layout/Footer.js
+++ Web/Fullstack-E-commerceApp/client/src/components/Layout/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <>
-      <h1>Footer</h1>
-    </>
+    <div className="bg-dark text-light p-3">
+      <h4 className="text-center">All Rigt Reserved &copy; Gani</h4>
+    </div>
   );
 };
 
