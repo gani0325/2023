79f91d0eaa100792280e6ccd27868da75d612581
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-03 01:15:33 +0900

    ➕[Feat] Layout의 Header.js

--- Web/Fullstack-E-commerceApp/client/src/components/Layout/Header.js
+++ Web/Fullstack-E-commerceApp/client/src/components/Layout/Header.js
@@ -1,9 +1,56 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <>
-      <h1>Header</h1>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <NavLink to="/" class="navbar-brand" href="#">
+              Hidden brand
+            </NavLink>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <NavLink
+                  to="/"
+                  class="nav-link active"
+                  aria-current="page"
+                  href="#"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink to="/register" class="nav-link" href="#">
+                  Register
+                </NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink to="/login" class="nav-link" href="#">
+                  Login
+                </NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink to="/cart" class="nav-link" href="#">
+                  Cart (0)
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
