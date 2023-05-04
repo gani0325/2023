615fcfd5561eea4bfee06e788a0b7ca683fe68ef
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-05 00:24:17 +0900

    🚧[Refactor] 페이지 찾을 수 없는 화면 꾸미기

--- Web/Fullstack-E-commerceApp/client/src/pages/Pagenotfound.js
+++ Web/Fullstack-E-commerceApp/client/src/pages/Pagenotfound.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Layout from "../components/Layout/Layout";
 
 const Pagenotfound = () => {
   return (
     <Layout>
-      <h1>Page not found</h1>
+      <div className="pnf">
+        <h1 className="pnf-title">404</h1>
+        <h2 className="pnf-heading">Oops ! Page not found!</h2>
+        <Link to="/" className="pnf-btn">
+          Go Back
+        </Link>
+      </div>
     </Layout>
   );
 };
