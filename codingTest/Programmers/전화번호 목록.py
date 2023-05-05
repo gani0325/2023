72a350e061577d74486c8f3b0419d9ca2ebb0c51
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-05 21:59:41 +0900

    ➕[Feat] 전화번호 목록

---
+++ codingTest/Programmers/전화번호 목록.py
@@ -0,0 +1,14 @@
+# 전화번호 목록
+# 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인
+
+# 전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때,
+# 어떤 번호가 다른 번호의 접두어라면 false, 그렇지 않으면 true를 return 하도록 solution 함수를 작성
+
+def solution(phone_book):
+    answer = True
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+            answer = False
+            break
+    return answer
