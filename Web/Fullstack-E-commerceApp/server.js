e1bf69b7ac87c0150934278e793844a491fa4e18
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-15 16:45:13 +0900

    ➕[Feat] category API

--- Web/Fullstack-E-commerceApp/server.js
+++ Web/Fullstack-E-commerceApp/server.js
@@ -3,38 +3,42 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRouter.js";
+import categoryRoutes from "./routes/categoryRouter.js";
+// import productRoutes from "./routes/productRouter.js";
+import cors from "cors";
 
-// router
-import authRouter from "./routes/authRouter.js";
-
-// configure env
+//configure env
 dotenv.config();
 
-// Databse config
+//databse config
 connectDB();
 
-// rest object
+//rest object
 const app = express();
 
-// middlewares
+//middelwares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-// routes
-app.use("/api/v1/auth", authRouter);
+//routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+// app.use("/api/v1/product", productRoutes);
 
-// rest api
+//rest api
 app.get("/", (req, res) => {
-  res.send("<h>Welcome ecommerce app</h>");
+  res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
-// PORT
-const PORT = process.env.PORT || 8000;
+//PORT
+const PORT = process.env.PORT || 8080;
 
-// run listen
+//run listen
 app.listen(PORT, () => {
   console.log(
-    `Server Running on ${process.env.DEV_MODE} http://localhost:${PORT}`.bgCyan
+    `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
       .white
   );
 });
