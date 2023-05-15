026752621dbd6cb0aca9f16957327f1a980fdeb1
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-15 16:44:08 +0900

    ➕[Feat] client "pages"

--- Web/Fullstack-E-commerceApp/client/src/pages/Pagenotfound.js
+++ Web/Fullstack-E-commerceApp/client/src/pages/Pagenotfound.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Layout from "../components/Layout/Layout";
+import Layout from "./../components/Layout/Layout";
 
 const Pagenotfound = () => {
   return (
-    <Layout>
+    <Layout title={"go back- page not found"}>
       <div className="pnf">
         <h1 className="pnf-title">404</h1>
-        <h2 className="pnf-heading">Oops ! Page not found!</h2>
+        <h2 className="pnf-heading">Oops ! Page Not Found</h2>
         <Link to="/" className="pnf-btn">
           Go Back
         </Link>
