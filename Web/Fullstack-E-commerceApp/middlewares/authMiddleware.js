194931484fa7ec7163087227ad7894c0add4a967
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-15 16:47:27 +0900

    🚧[Refactor] 연산자 수정

--- Web/Fullstack-E-commerceApp/middlewares/authMiddleware.js
+++ Web/Fullstack-E-commerceApp/middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 import JWT from "jsonwebtoken";
 import User from "../models/User.js";
 
-// Protected Routes token base
+//Protected Routes token base
 export const requireSignIn = async (req, res, next) => {
   try {
     const decode = JWT.verify(
@@ -15,12 +15,12 @@ export const requireSignIn = async (req, res, next) => {
   }
 };
 
-// admin access
+// admin acceess
 export const isAdmin = async (req, res, next) => {
   try {
     const user = await User.findById(req.user._id);
     // role이 0 이면 일반 & 1 이면 admin
-    if (!user.role !== 1) {
+    if (user.role !== 1) {
       return res.status(401).send({
         success: false,
         message: "UnAuthorized Access",
