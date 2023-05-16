bd6725ffa58b2bcf72608da9e503280c0bb460cb
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-16 15:58:45 +0900

    ➕[Feat] product API

--- Web/Fullstack-E-commerceApp/server.js
+++ Web/Fullstack-E-commerceApp/server.js
@@ -5,7 +5,7 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRouter.js";
 import categoryRoutes from "./routes/categoryRouter.js";
-// import productRoutes from "./routes/productRouter.js";
+import productRoutes from "./routes/productRouter.js";
 import cors from "cors";
 
 //configure env
@@ -25,7 +25,7 @@ app.use(morgan("dev"));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
-// app.use("/api/v1/product", productRoutes);
+app.use("/api/v1/product", productRoutes);
 
 //rest api
 app.get("/", (req, res) => {
