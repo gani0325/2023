4e2aa26cb76ca2e35a51c0a3133d2382ddffef41
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-18 18:54:48 +0900

    ➕[Feat] axios로 서버에 get, post 요청하기

--- Web/Back_Server/client/src/App.js
+++ Web/Back_Server/client/src/App.js
@@ -1,36 +1,33 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+
 // 기본 제공하는 api는 fetch
 // axios 라이브러리
 
+const SERVER_URL = "http://localhost:8080/api/todo";
 // 프론트가 서버에 데이터 요청하려면 "서버주소", "http 메소드" 필요
 function App() {
   const [todoList, setTodoList] = useState([]);
+  // axios 이용하여 데이터 응답 받기
+  const fetchData = async () => {
+    // 응답 보내기 -> 응답 오면 setTolist에 넣기
+    const response = await axios.get(SERVER_URL);
+    setTodoList(response.data);
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
 
-  // fetch 이용하여 데이터 응답 받기
-  const fetchData = () => {
-    fetch("http://localhost:8080/api/todo")
-      .then((response) => response.json())
-      .then((data) => setTodoList(data));
-  };
-
   // 데이터 추가하고 서버에 POST 요청
-  const onSubmitHandler = (e) => {
+  const onSubmitHandler = async (e) => {
     e.preventDefault();
     const text = e.target.text.value;
     const done = e.target.done.checked;
-    fetch("http://localhost:8080/api/todo", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        text,
-        done,
-      }),
-    }).then(() => fetchData());
+
+    // axios 이용하여 데이터 보내기
+    await axios.post(SERVER_URL, { text, done });
+    fetchData();
   };
 
   return (
