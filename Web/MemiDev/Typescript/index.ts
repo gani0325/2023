df1a582cc5ff5ca70afaa8d469167b4a56b2e9ce
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-26 18:04:17 +0900

    ➕[Feat] Nodejs Buffer 이해하기

--- Web/MemiDev/Typescript/index.ts
+++ Web/MemiDev/Typescript/index.ts
@@ -1,22 +1,16 @@
-// 1
-let a: number | string = 3434;
-a = "abd"
-console.log(a)
+// buffer 알아보기
+import {Buffer} from "node:buffer";
+const arr = [];
 
-// 2
-const sum = (a : number, b:number) => a+b;
-console.log(sum(4, 5));
+arr.push(1);
+arr.push(3);
+arr.push(5);
+arr.push(7);
+console.log(arr);       // [ 1, 3, 5, 7 ]
 
-// 3
-interface Studnet {
-    name:string
-    age:number
-};
-const student: Studnet = {
-    name : "hi", age: 24
-};
-const change = (x:Studnet) => {
-    x.name ="change"
-    x.age = 44
-};
-console.log(student)
+const a = Buffer.from([2, 3, 4, 5]);
+console.log(a, a.length);       // <Buffer 02 03 04 05> 4
+const b = Buffer.alloc(10);
+console.log(b, b.length);       // <Buffer 00 00 00 00 00 00 00 00 00 00> 10
+const c = Buffer.allocUnsafe(10)
+console.log(c, c.length)        // <Buffer f0 0f f6 05 00 00 00 00 08 59> 10
