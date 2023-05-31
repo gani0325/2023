c32668f672920e7ba488f51af059b9763f63061a
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-31 17:49:21 +0900

    ➕[Feat] express로 html 보내기

--- Web/MemiDev/test-socket-server/index.ts
+++ Web/MemiDev/test-socket-server/index.ts
@@ -1,9 +1,13 @@
 import express from "express"
 import birds from "./routes/birds"
 import dogs from "./routes/dogs"
+import fs from "node:fs/promises"
 const app = express()
 
 app.use("/birds", birds)
 app.use("/dogs", dogs)
-
+app.use("/", async (req, res) => {
+  const b = await fs.readFile("./public/index.html")
+  res.send(b.toString())
+})
 app.listen(4000)
