41e46b498ae34587dbfad522e974e7e1795c6de7
gani0325 <lge0325@bme.gachon.ac.kr>
2023-05-31 18:05:52 +0900

    ➕[Feat] static 으로 정적인 html 불러오기

--- Web/MemiDev/test-socket-server/index.ts
+++ Web/MemiDev/test-socket-server/index.ts
@@ -4,10 +4,11 @@ import dogs from "./routes/dogs"
 import fs from "node:fs/promises"
 const app = express()
 
+app.use("/", express.static("public"))
 app.use("/birds", birds)
 app.use("/dogs", dogs)
 app.use("/", async (req, res) => {
-  const b = await fs.readFile("./public/index.html")
+  const b = await fs.readFile("./html/index.html")
   res.send(b.toString())
 })
 app.listen(4000)
