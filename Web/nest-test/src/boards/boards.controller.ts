f734d54808c29bbc1aba149a7b05e81297c1c38a
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-01 00:57:02 +0900

    ➕[Feat] 컨트롤러에서는 요청을 처리하고 결과값을 리턴함

---
+++ Web/nest-test/src/boards/boards.controller.ts
@@ -0,0 +1,15 @@
+// 들어오는 요청을 처리하고 클라이언트에 응답을 반환함
+// @Controller 데코레이터로 클래스를 데코레이션하여 정의함
+// Handler : @Get, @Post, @Delete 등과 같은 데코레이터로 장식 된 컨트롤러 클래스 내의 단순한 메서드
+import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+    constructor(private boardsService : BoardsService) {}
+        
+    @Get("/")
+    getAllBoard() {
+        return this.boardsService.getAllBoards();
+    }
+}
