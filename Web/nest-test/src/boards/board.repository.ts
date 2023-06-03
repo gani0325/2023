b32a300737dca16f1b32dc1e861aca7c8ddb6c0d
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-03 22:40:01 +0900

    ➕[Feat] Repository 생성하기

---
+++ Web/nest-test/src/boards/board.repository.ts
@@ -0,0 +1,5 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Board } from "./boards/board.entity";
+
+@EntityRepository(Board)
+export class BoardRepository extends Repository<Board> {}
