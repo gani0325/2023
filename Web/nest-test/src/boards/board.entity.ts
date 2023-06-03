79dfdab767e648f399ec8d148af89681a1ffe250
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-03 23:26:33 +0900

    🚧[Refactor] 엔티티 설정하기

--- Web/nest-test/src/boards/board.entity.ts
+++ Web/nest-test/src/boards/board.entity.ts
@@ -1,6 +1,8 @@
-import { BaseEntity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 import { BoardStatus } from "./board-status.enum";
 
+// Board 클래스가 엔티티임을 나타내는데 사용
+@Entity()
 export class Board extends BaseEntity {
     @PrimaryGeneratedColumn()
     id:number;
