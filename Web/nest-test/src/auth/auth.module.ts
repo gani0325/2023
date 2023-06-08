2de49d236e1bbeacbb398ade300508ae6b6b8d02
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-08 17:48:33 +0900

    ➕[Feat] 로그인 성공 시 JWT 이용해서 토큰 생성하기

--- Web/nest-test/src/auth/auth.module.ts
+++ Web/nest-test/src/auth/auth.module.ts
@@ -3,9 +3,20 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
+    PassportModule.register({ defaultStrategy: "jwt"}),
+    JwtModule.register({
+      // 토큰을 만들 때 이용하는 Secret 텍스트
+      secret: "Secret1234", // 아무거나 해도 됨
+      signOptions: {
+        // 정해진 시간 이후에는 토큰이 유효하지 않게 됨 (60 * 60 은 한시간)
+        expiresIn: 60 * 60,
+      }
+    }),
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
