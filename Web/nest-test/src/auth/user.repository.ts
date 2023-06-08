a0adc6814be3f9cb75cf4e883584c7672fea6e9e
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-08 17:05:36 +0900

    ➕[Feat] 데이터베이스 레벨에서 같은 이름을 가진 유저가 있다면 에러

--- Web/nest-test/src/auth/user.repository.ts
+++ Web/nest-test/src/auth/user.repository.ts
@@ -1,13 +1,23 @@
-import { EntityRepository, Repository } from "typeorm";
+import { CustomRepositoryCannotInheritRepositoryError, EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
         const user = this.create({ username, password });
-
-        await this.save(user);
+        
+        // 데이터베이스 레벨에서 만약 같은 이름을 가진 유저가 있다면 에러 던지기 
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === "23505") {
+                throw new ConflictException("Existing username");
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
