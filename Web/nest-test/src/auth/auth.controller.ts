c1d096d06bd37982da10933ab4620be104802fe8
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-08 16:52:02 +0900

    ➕[Feat] ValidationPipe 로 Dto 에 있는 유효성 조건에 맞게 체크

--- Web/nest-test/src/auth/auth.controller.ts
+++ Web/nest-test/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
@@ -8,7 +8,8 @@ export class AuthController {
     
     // localhost:3000/auth/signUp
     @Post("/signup")
-    signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    // ValidationPipe : 요청이 컨트롤러에 있는 핸들러로 들어왔을 때 Dto 에 있는 유효성 조건에 맞게 체크
+    signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authCredentialsDto);
     }
 }
