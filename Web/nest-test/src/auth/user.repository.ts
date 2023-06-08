e944924d70384f981f69b3e6aabc7304df73cd93
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-08 17:09:05 +0900

    ➕[Feat] bcrypt 로 비밀번호를 암호화 한 후 데이터베이스에 저장하기

--- Web/nest-test/src/auth/user.repository.ts
+++ Web/nest-test/src/auth/user.repository.ts
@@ -1,13 +1,17 @@
-import { CustomRepositoryCannotInheritRepositoryError, EntityRepository, Repository } from "typeorm";
+import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 import { ConflictException, InternalServerErrorException } from "@nestjs/common";
-
+import * as bcrypt from "bcryptjs";
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
-        const user = this.create({ username, password });
+
+        // bcrypt 로 비밀번호를 암호화 한 후 데이터베이스에 저장하기
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const user = this.create({ username, password: hashedPassword});
         
         // 데이터베이스 레벨에서 만약 같은 이름을 가진 유저가 있다면 에러 던지기 
         try {
