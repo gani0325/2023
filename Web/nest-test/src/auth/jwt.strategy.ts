be6858c2cd74b4fb60a032e2b2d5757b57b0ee8c
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-09 11:11:56 +0900

    ➕[Feat]  Passport, JWT 를 이용해서 토큰 인증 후 유저 정보 가져오기

---
+++ Web/nest-test/src/auth/jwt.strategy.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { InjectRepository } from "@nestjs/typeorm";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UserRepository } from "./user.repository";
+import { User } from "./user.entity";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository: UserRepository
+    ) {
+        super({
+            // JWT token in order to validate it
+            // Access its payload
+            secretOrKey: "Secret1234",
+            // to look for the JWT in the Authorization Header of current Request
+            // passed over as a Bearer token
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+        })
+    }
+
+    // 토큰이 유효한지 체크가 되면 validate 메소드에서 payload 에 있는 유저 이름이
+    // 데이터베이스에서 있는 유저인지 확인! 있다면 유저 객체 return
+    // return @UseGuards(AuthGuard()) 이용한 모든 요청의 Request Object에 들어감
+    async validate(payload) {
+        const {username} = payload;
+        const user: User = await this.userRepository.findOne({username});
+
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+
+        return user;
+    }
+}
