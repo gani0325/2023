c41ff30f8e9bf9ca4d5db9a1e72ab2f891b01469
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-15 20:31:29 +0900

    ➕[Feat] key 설정하기

---
+++ Web/CryptocurrencyPrices/commands/key.js
@@ -0,0 +1,13 @@
+const key = {
+  set() {
+    console.log("Hello frm set");
+  },
+  show() {
+    console.log("Hello frm set");
+  },
+  remove() {
+    console.log("Hello frm set");
+  },
+};
+
+module.exports = key;
