09c793bb9e287a75b4a11f8b5395798383389834
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-16 18:22:50 +0900

    ➕[Feat] CREATE a stores

--- Web/StoreLocationAPI/controllers/stores.js
+++ Web/StoreLocationAPI/controllers/stores.js
@@ -17,3 +17,24 @@ exports.getStores = async (req, res, next) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
+// @desc CREATE a stores
+// @route POST /api/v1/stores
+// @access Public
+exports.addStore = async (req, res, next) => {
+  try {
+    // { storeId: '0001', address: '10 main st' }
+    const store = await Store.create(req.body);
+
+    return res.status(200).json({
+      success: true,
+      data: store,
+    });
+  } catch (err) {
+    console.error(err);
+    if (err.code === 11000) {
+      return res.status(400).json({ error: "This store already exists" });
+    }
+    res.status(500).json({ error: "Server error" });
+  }
+};
