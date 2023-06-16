a27b60a1760d533266d529b8ac425e3bdb01672b
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-16 18:17:35 +0900

    ➕[Feat] GET all stores

--- Web/StoreLocationAPI/controllers/stores.js
+++ Web/StoreLocationAPI/controllers/stores.js
@@ -1,7 +1,19 @@
+const Store = require("../models/Store");
+
 // @desc GET all stores
 // @route GET /api/v1/stores
 // @access Public
+exports.getStores = async (req, res, next) => {
+  try {
+    const stores = await Store.find();
 
-exports.getStores = (req, res, next) => {
-  res.send("Hello");
+    return res.status(200).json({
+      success: true,
+      count: stores.length,
+      data: stores,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
 };
