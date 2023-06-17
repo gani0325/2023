bcdbad80b32c8c6b8775893c5471d0e43e5f3cec
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-17 21:57:20 +0900

    ➕[Feat] room ID 로 넘어가는 API 구성

--- Web/ZoomClone/server.js
+++ Web/ZoomClone/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+// UUID : 범용 고유 식별자를 의미하며 중복이 되지 않는 유일한 값을 구성
+const { v4: uuidv4 } = require("uuid");
+
+// EJS
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello world");
+  res.redirect(`/${uuidv4()}`);
 });
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
 server.listen(3030);
