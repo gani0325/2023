0b8b3d2192d7ae4f82150f380e79539a46af70bc
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-17 22:15:25 +0900

    ➕[Feat] 줌에서 식별자 UUID 사용하기

--- Web/ZoomClone/server.js
+++ Web/ZoomClone/server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 // UUID : 범용 고유 식별자를 의미하며 중복이 되지 않는 유일한 값을 구성
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidV4 } = require("uuid");
 
 // EJS
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.redirect(`/${uuidv4()}`);
+  res.redirect(`/${uuidV4()}`);
 });
 
 app.get("/:room", (req, res) => {
