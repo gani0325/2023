e43efe0035e96460210824258b5e0f9358c64f8d
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-19 20:01:34 +0900

    ➕[Feat] session Id 알아보기

--- Web/AuthNode/server.js
+++ Web/AuthNode/server.js
@@ -1,7 +1,23 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 
+// 모든 uri에 접근 했을 때 적용되도록 라우터를 만듦
+app.use(
+  session({
+    // 암호화에 대한 내용
+    secret: "key that all sign cookie",
+    // 매 request 마다 세션을 계속 다시 저장
+    resave: false,
+    // request에서 새로 생성된 session에 아무런 작업이 이루어지지 않은 상황
+    saveUninitialized: false,
+  })
+);
+
 app.get("/", (req, res) => {
+  req.session.isAuth = true;
+  console.log(req.session);
+  console.log(req.session.id);
   res.send("Hello sessions");
 });
 
