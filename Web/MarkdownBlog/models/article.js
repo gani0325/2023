7e28923a3a11bfe11ff3d0544ead586b341e5c37
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-22 01:34:43 +0900

    ➕[Feat] xss 공격 방지

--- Web/MarkdownBlog/models/article.js
+++ Web/MarkdownBlog/models/article.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const marked = require("marked");
 const slugify = require("slugify");
+const createdDomPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const dompurify = createdDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
   title: {
@@ -24,12 +27,20 @@ const articleSchema = new mongoose.Schema({
     requied: true,
     unique: true,
   },
+  sanitizedHtml: {
+    type: String,
+    requied: true,
+  },
 });
 
 articleSchema.pre("validate", function (next) {
   // title 명으로 넘어가기
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true });
+  }
+
+  if (this.markdown) {
+    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
   }
   next();
 });
