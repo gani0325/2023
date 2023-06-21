ed2718fbb733643a6395f4e5b908e4984f5cbd62
gani0325 <lge0325@bme.gachon.ac.kr>
2023-06-22 00:05:12 +0900

    ➕[Feat] Create Article Router

--- Web/MarkdownBlog/server.js
+++ Web/MarkdownBlog/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const articleRouter = require("./routes/articles");
 const app = express();
 
 app.set("view engine", "ejs");
+
+// router
+app.use("/article", articleRouter);
 
 app.get("/", (req, res) => {
   res.render("index");
