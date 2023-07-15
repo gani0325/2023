fbbfc5d699aedcc8ae29462c04e818fc828ea6d1
gani0325 <lge0325@bme.gachon.ac.kr>
2023-07-16 00:28:05 +0900

    ➕[Feat] String 배열정렬 (문자열 분리 & 문자 앞뒤 공백 제거)

--- codingTest/JAVA/StringMethod.java
+++ codingTest/JAVA/StringMethod.java
@@ -7,26 +7,11 @@ public class main {
     public static void main(String[] args) {
         String str = "apple";
 
-        // 문자 동일 여부 판단
-        // 자바 string의 경우, 클래스로써 Call by Reference형태로 생성 시 주소값이 부여
-        // => String타입을 선언했을때는 같은 값을 부여하더라도 서로간의 주소값이 다르다.
-        // => 값 비교로는 equals를 사용
-        System.out.println(str.equals("apple"));
+        // 문자열 분리
+        System.out.println(str.split(" ")); // 공백으로 구분된 문자열 str을 분리하여 String[] 배열로 반환
+        System.out.println(str.split()); // 띄어쓰기 없는 문자열 str을 한 문자씩 분리하여 String[] 배열로 반환
 
-        // 문자 비교
-        /**
-         * str과 같으면 0
-         * str이 사전순으로 앞이면 -1
-         * str이 사전순으로 뒤면 1
-         * str이 마지막 문자만 다르면, 마지막 문자의 사전순 차이 반환
-         */
-        System.out.println(str.compareTo("applq"));
-
-        // 문자 포함 여부 판단
-        System.out.println(str.contains("app"));
-
-        // 문자 <-> 숫자 변환
-        System.out.println(Integer.parseInt("100")); // 문자열 "100"을 숫자 100으로 변환
-        System.out.println(Integer.toString(100)); // 숫자 100을 문자열 "100"으로 변환
+        // 문자 앞뒤 공백 제거
+        System.out.println(str.trim()); // str의 앞뒤 공백을 제거한다. 문자열 사이의 공백은 제거하지 않는다.
     }
 }
