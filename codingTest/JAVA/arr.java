8ec789ac69756975ae945795a9043676a7605705
gani0325 <lge0325@bme.gachon.ac.kr>
2023-07-15 23:44:27 +0900

    ➕[Feat] 배열정렬 (오름차순)

--- codingTest/JAVA/arr.java
+++ codingTest/JAVA/arr.java
@@ -5,10 +5,10 @@ import java.io.*;
 public class main {
     // tip: arguments are passed via the field below this editor
     public static void main(String[] args) {
-        int[] arr = { 1, 26, 17, 25, 99, 44, 303 };
+        Integer[] arr = { 1, 26, 17, 25, 99, 44, 303 };
 
         // 변수 arr의 순서를 변경해주기 때문에 리턴 값을 다른 변수에 할당할 필요가 없음
-        Arrays.sort(arr);
-        System.out.println("오름차순 : " + Arrays.toString(arr));
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println("내림차순 : " + Arrays.toString(arr));
     }
 }
