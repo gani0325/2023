86258f115987b9cbf1529862712772cfaed74bb8
gani0325 <lge0325@bme.gachon.ac.kr>
2023-07-16 15:41:29 +0900

    ➕[Feat] StringBuilder 배열정렬 (문자열 추가 및 뒤집기 및 절대길이 & 특정 인덱스 문자변경)

--- codingTest/JAVA/StringBuilderMethod.java
+++ codingTest/JAVA/StringBuilderMethod.java
@@ -7,14 +7,18 @@ public class main {
         // 문자열 추가
         System.out.println(sb.append("apple")); // "apple"
 
-        // 특정 인덱스에 문자 삽입
-        System.out.println(sb.insert(2, "oo")); // "apoople"
+        // 특정 인덱스의 문자를 변경
+        sb.setCharAt(1, 'e');
 
-        // 문자열 삭제
-        System.out.println(sb.delete(0, 2)); // "oople"
+        // 문자열 뒤집기
+        System.out.println(sb.reverse()); // "elpo"
 
-        // 특정 인덱스의 문자 삭제
-        System.out.println(sb.deleteCharAt(2)); // "oole"
+        // 문자열 절대길이 줄이기
+        sb.setLength(2); // "el"
 
+        // 문자열 절대길이 늘이기
+        sb.setLength(4); // "el " -> 뒤가 공백으로 채워짐
+
+        System.out.println(sb);
     }
 }
