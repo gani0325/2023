39be927d76b3f669223df8408c01446152a59222
gani0325 <lge0325@bme.gachon.ac.kr>
2023-07-16 16:14:08 +0900

    ➕[Feat] ListWithArrary 배열정렬 (Array 와 List 변환)

--- codingTest/JAVA/ArraryWithList.java
+++ codingTest/JAVA/ArraryWithList.java
@@ -5,13 +5,14 @@ import java.io.*;
 public class main {
     // tip: arguments are passed via the field below this editor
     public static void main(String[] args) {
-        // asList : 정수 배열을 List로 변환
-        int[] temp = { 1, 2, 3, 4 };
-        List<Integer> list = new ArrayList<>(Arrays.asList(temp));
+        // asList : 문자열 타입 배열을 List로 변환
+        String[] temp = { "apple", "banana", "lemon" };
+        List<String> list = new ArrayList<>(Arrays.asList(temp));
         System.out.println(list);
 
-        // toArray : List를 정수 배열로 변환
-        List<Integer> list = new ArrayList<>();
-        int[] temp = list.stream().mapToInt(x -> x).toArray();
+        // toArray : List를 문자열 배열로 변환
+        List<String> list = new ArrayList<>();
+        String[] temp = list.toArray(new String[list.size()]);
+        System.out.println(temp);
     }
 }
