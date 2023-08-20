e022bf8d576600c8f8c07db6ea67a1ebe44d3a5f
gani0325 <lge0325@bme.gachon.ac.kr>
2023-08-20 23:58:28 +0900

    ➕[Feat] Prevent deplicate emails

--- Web/PHP_MySQL/process-signup.php
+++ Web/PHP_MySQL/process-signup.php
@@ -12,12 +12,33 @@ if (strlen($_POST["password"]) < 8) {
     die("Password must be at least 8 characters");
 }
 
-if (!preg_match("/[a-z]/i"), $_POST["password"]) {
+if (! preg_match("/[a-z]/i"), $_POST["password"]) {
     die("Password must contain at least one letter");
 }
 
-if (!preg_match("/[0-9]/"), $_POST["password"]) {
+if (! preg_match("/[0-9]/"), $_POST["password"]) {
     die("Password must contain at least one letter");
 }
+
+if ($_POST["password"] !== $_POST["password_confirmation"]) {
+    die("Password must match");
+}
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$mysqli = required __DIR__ . "/database.php";
+$sql = "INSERT INTO user (name, email, password_hash)
+        VALUES (?, ?, ?)";
+
+$stmp = $mysqli->stmt_init();
+if (! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",
+                $_POST["name"],
+                $POST["email"],
+                $password_hash);
 
 print_r($_POST);
+var_dump($password_hash);
