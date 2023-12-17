03da6de246aa90e19e6d71d7271d2fd3466f2c39
gani0325 <lge0325@bme.gachon.ac.kr>
2023-12-17 19:37:22 +0900

    ➕[Feat] 게임 맵 최단거리

--- codingTest/Programmers/게임 맵 최단거리.py
+++ codingTest/Programmers/게임 맵 최단거리.py
@@ -1,41 +1,63 @@
-# 게임 맵 최단거리
-# ROR 게임은 두 팀으로 나누어서 진행하며, 상대 팀 진영을 먼저 파괴하면 이기는 게임
-# 각 팀은 상대 팀 진영에 최대한 빨리 도착하는 것이 유리
-# 검은색 부분은 벽으로 막혀있어 갈 수 없는 길이며, 흰색 부분은 갈 수 있는 길
-# 캐릭터가 움직일 때는 동, 서, 남, 북 방향으로 한 칸씩 이동하며, 게임 맵을 벗어난 길은 갈 수 없음
+"""
+게임 맵 최단거리
 
-# 게임 맵의 상태 maps가 매개변수로 주어질 때, 캐릭터가 상대 팀 진영에 도착하기 위해서 
-# 지나가야 하는 칸의 개수의 최솟값을 return 하도록 solution 함수
-# 상대 팀 진영에 도착할 수 없을 때는 -1을 return
+💛 문제
+ROR 게임은 두 팀으로 나누어서 진행하며, 상대 팀 진영을 먼저 파괴하면 이기는 게임입니다.
+따라서, 각 팀은 상대 팀 진영에 최대한 빨리 도착하는 것이 유리합니다.
 
-# n x m 크기의 게임 맵의 상태
-# 0은 벽이 있는 자리, 1은 벽이 없는 자리
+게임 맵의 상태 maps가 매개변수로 주어질 때, 
+캐릭터가 상대 팀 진영에 도착하기 위해서 지나가야 하는 칸의 개수의 최솟값을 return 하도록 solution 함수를 완성해주세요. 
+단, 상대 팀 진영에 도착할 수 없을 때는 -1을 return 해주세요.
+
+🧡 제한 사항
+maps는 n x m 크기의 게임 맵의 상태가 들어있는 2차원 배열로, n과 m은 각각 1 이상 100 이하의 자연수입니다.
+    n과 m은 서로 같을 수도, 다를 수도 있지만, n과 m이 모두 1인 경우는 입력으로 주어지지 않습니다.
+maps는 0과 1로만 이루어져 있으며, 0은 벽이 있는 자리, 1은 벽이 없는 자리를 나타냅니다.
+처음에 캐릭터는 게임 맵의 좌측 상단인 (1, 1) 위치에 있으며, 상대방 진영은 게임 맵의 우측 하단인 (n, m) 위치에 있습니다.
+
+💚 입출력
+maps	                                                        answer
+[[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]	11
+[[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,0],[0,0,0,0,1]]	-1
+"""
+
 from collections import deque
 
-def solution(maps):
+def bfs(maps, x, y):
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
     queue = deque()
-    n = len(maps)
-    m = len(maps[0])
-    
-    # 캐릭터는 게임 맵의 좌측 상단인 (1, 1)
-    # 상대방 진영은 게임 맵의 우측 하단인 (n, m) 
-    dx = [-1, 0, 1, 0]
-    dy = [0, 1, 0, -1]
-    queue.append((0, 0))
-    answer = 0
-    
-    while queue :
+    queue.append((x, y))
+
+    while queue:
         x, y = queue.popleft()
-        for i in range(4) :
+
+        for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and maps[nx][ny] == 1 :
+            
+            if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]): 
+                continue
+            # 벽
+            if maps[nx][ny] == 0: 
+                continue
+
+            # 처음 지나가는 길 + 거리 계산
+            if maps[nx][ny] == 1:
                 maps[nx][ny] = maps[x][y] + 1
                 queue.append((nx, ny))
-
-    if maps[n-1][m-1] == 1 :
-        answer = -1
-    else :
-        answer = maps[n-1][m-1]
     
-    return answer
+    # 상대편 좌표까지의 거리 (n, n)
+    return maps[len(maps)-1][len(maps[0])-1]
+    
+# maps는 0과 1로만 이루어져 있으며, 0은 벽이 있는 자리, 1은 벽이 없는 자리
+def solution(maps):
+    answer = 0
+    answer = bfs(maps, 0, 0)
+    
+    if answer == 1 :
+        return -1
+    else :
+        return answer
